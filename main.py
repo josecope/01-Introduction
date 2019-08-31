96b555619f451f399c155e959be951b14dc26ee3
Joey Copeland <joeycopeland@Joeys-MBP.hsd1.in.comcast.net>
2019-08-31 18:58:01 -0500

    Finished project.

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello!  My name is Joey Copeland.')
+print('My favorite game is Star Wars Knights of the Old Republic 2: The Sith Lords.  It is actually one of the first games I ever played also.')
+print('In this class, I am most worried about not being able to memorize commands and implement them in different ways.')
+print('However, I am very excited to learn more of the "behind the scenes" work that goes into a game.  I am very excited to make a game too!')
+print('My stackoverflow user number is 11996695')
+print('Here is the link to my github profile! https://github.com/josecope')
+print('Thanks for reading.')
